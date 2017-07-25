6fcc5e17fd4b207d85724c331278acf8382f4af8
angaoei <angaoei@gmail.com>
2017-07-26 06:32:42 +0800

    改为标准折线图样式

--- OutputCollect/ViewController.m
+++ OutputCollect/ViewController.m
@@ -44,17 +44,23 @@
     PYOption *option = [PYOption initPYOptionWithBlock:^(PYOption *option) {
         option.titleEqual([PYTitle initPYTitleWithBlock:^(PYTitle *title) {
             title.textEqual(@"宁晋电池产出比例各车间综合数据趋势图")
-            .textStyle.fontSizeEqual(@10);
+            .textStyle.fontSizeEqual(@16);
             title.xEqual(PYPositionCenter).yEqual(PYPositionTop);
             
         }])
+        .gridEqual([PYGrid initPYGridWithBlock:^(PYGrid *grid) {
+            grid.xEqual(@40).x2Equal(@90).yEqual(@50);
+        }])
         .tooltipEqual([PYTooltip initPYTooltipWithBlock:^(PYTooltip *tooltip) {
-            tooltip.triggerEqual(PYTooltipTriggerAxis);
+            tooltip.triggerEqual(PYTooltipTriggerAxis)
+            .textStyleEqual([PYTextStyle initPYTextStyleWithBlock:^(PYTextStyle *textStyle) {
+                textStyle.fontSizeEqual(@8);
+            }]);
         }])
         .legendEqual([PYLegend initPYLegendWithBlock:^(PYLegend *legend) {
             legend.dataEqual(@[@"156SK-PR2",@"156SK-PR硼",@"156SK-PR",@"156MK-RE",
                                @"156MK-C5-4BB",@"156MK-C6-5BB",@"156MK-JG-4BB",@"156MK-JG-5BB"]);
-            legend.textStyle.fontSizeEqual(@6);
+            legend.textStyle.fontSizeEqual(@8);
             legend.xEqual(PYPositionRight);
             legend.yEqual(PYPositionCenter);
             legend.orientEqual(PYOrientVertical);
@@ -71,58 +77,59 @@
             axis.typeEqual(PYAxisTypeValue).boundaryGapEqual(@NO)
             .minEqual(@0.0)
             .maxEqual(@100.0)
+            .splitNumberEqual(@4)
             .axisLabelEqual([PYAxisLabel initPYAxisLabelWithBlock:^(PYAxisLabel *axisLabel) {
                 axisLabel.formatterEqual(@"{value} %");
             }])
             .axisLine.lineStyle.widthEqual(@1.5);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156SK-PR2")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(96.79),@(84.66),@(85.30),@(92.36)]);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156SK-PR硼")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(85.79),@(74.66),@(95.30),@(72.36)]);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156SK-PR")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(93.79),@(84.66),@(85.30),@(92.36)]);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156MK-RE")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(70.79),@(94.66),@(75.30),@(82.36)]);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156MK-C5-4BB")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(92.79),@(84.66),@(95.30),@(92.36)]);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156MK-C6-5BB")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(95.79),@(94.66),@(95.30),@(82.36)]);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156MK-JG-4BB")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(88.79),@(94.66),@(85.30),@(72.36)]);
         }])
-        .addSeries([PYCartesianSeries initPYCartesianSeriesWithBlock:^(PYCartesianSeries *series) {
-            series.stackEqual(@"总量")
+        .addSeries([PYSeries initPYSeriesWithBlock:^(PYSeries *series) {
+            series.nameEqual(@"总量")
             .nameEqual(@"156MK-JG-5BB")
             .typeEqual(PYSeriesTypeLine)
-            .dataEqual(@[@(96.79),@(94.66),@(95.30),@(92.36)]);
+            .dataEqual(@[@(91.79),@(74.66),@(85.30),@(92.36)]);
         }]);
     }];
     
